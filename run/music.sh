1202ec200f219f2a721f45d215a55492e086fe09
Dark2932 <72404859+Dark2932@users.noreply.github.com>
2022-01-30 00:22:14 +0800

    Update

--- run/music.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -x
-
-[ -d "${GITHUB_WORKSPACE}" ] && PROJECT_DIR="${GITHUB_WORKSPACE}"
-[ "${PROJECT_DIR}" != "${PWD}" ] && cd "${PROJECT_DIR}"
-
-mkdir artifacts
-mv ProjectTEP/music/* artifacts
